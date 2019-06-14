882b94190a6986977e127a4fea3f32de33f26f9e
Pauline Sauget <paulinevsauget@gmail.com>
2019-06-14 15:27:46 -0700

    Convert Track to class. Add state: favorite, toggled on/off onChange

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,34 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import "./styles/Track.css";
 
 // Here we use destructuring to extract the props into separate variables
 // See https://wesbos.com/destructuring-objects/
-const Track = ({title, artist, playtime, albumart, favorite}) => {
-  return (
-    <li className="track">
-      <img className="track--albumart" alt={`album art for ${title}`} src={albumart} />
-      <h3 className="track--title">{title}</h3>
-      <input
-        type="checkbox"
-        className="track--favorite"
-        checked={!favorite}
-      />
-      <p className="track--artist">{artist}</p>
-      <p className="track--playtime">{playtime}</p>
-      <button
-        className="track--control track--to-top"
-        >
-        <span role="img" aria-label="send to top">🔝</span>
-      </button>
-      <button
-        className="track--control track--switch"
-        >
-        <span role="img" aria-label="switch lists">↔</span>
-      </button>
-    </li>
-  );
+
+// const Track = ({title, artist, playtime, albumart, favorite}) => {
+class Track extends Component {
+  constructor(props) {
+   super(props);
+   this.state = { favorite: false }; 
+  };
+
+  onChange = () => {
+    console.log(this)
+    this.setState({ favorite: !this.state.favorite })
+  }
+
+  render () {
+    return (
+      <li className="track">
+        <img className="track--albumart" alt={`album art for ${this.props.title}`} src={this.albumart} />
+        <h3 className="track--title">{this.props.title}</h3>
+        <input
+          type="checkbox"
+          className="track--favorite"
+          checked={!this.state.favorite}
+          onChange={this.onChange}
+        />
+        <p className="track--artist">{this.props.artist}</p>
+        <p className="track--playtime">{this.props.playtime}</p>
+        <button
+          className="track--control track--to-top"
+          >
+          <span role="img" aria-label="send to top">🔝</span>
+        </button>
+        <button
+          className="track--control track--switch"
+          >
+          <span role="img" aria-label="switch lists">↔</span>
+        </button>
+      </li>
+    );
+  }
 };
 
 Track.propTypes = {
